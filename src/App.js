8e125442cef96a701e0fcc64fb3fc7995d0c2443
YerkoMedinaPichun <yrk.27.medina@gmail.com>
2022-11-08 20:56:22 -0300

    CRUD con Reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Contador from "./components/Contador";
 import ContadorMejorado from "./components/ContadorMejorado";
+import CrudApi from "./components/CrudApi";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <CrudApi />
+      <hr />
       <ShoppingCart />
+      <hr />
       <ContadorMejorado />
       <hr />
       <Contador />
